890e604f4855b23135f823d8d2a1eb2c18764fce
lumi1100 <qz661137@dal.com>
2025-09-17 17:18:05 -0300

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@ console.log("Thank you for visiting. I look forward to joining your company!");
 // Visitor counter logic
 async function updateCounter() {
   try {
-    const res = await fetch("https://foywu6wvypemtwxj473rwme6zy0vbidu.lambda-url.us-east-2.on.aws/", { method: "GET" });
+    const res = await fetch("https://kj3shuyqpsphmty7aniyawkan40lxbfb.lambda-url.us-east-2.on.aws/", { method: "GET" });
     const data = await res.json();
     const el = document.getElementById("visitor-count");
     el.textContent = Number(data.count).toLocaleString();
